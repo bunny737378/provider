9e5a3398ab70b4aa97b0f5da31c86a5039779a28
bunny737378 <bhupendrasahu69.com@gmail.com>
2026-07-30 14:00:43 +0530

    Update streams.js

--- moviebox/streams.js
+++ moviebox/streams.js
@@ -1,3 +1,6 @@
+// ============================================
+// STREAM HANDLER
+// ============================================
 function handleStreams(req) {
     let p = (req.id || "").split("--");
     if (p.length != 2) return [];
@@ -34,6 +37,60 @@ function handleStreams(req) {
         play.data.streams.forEach(s => {
             out.push({
                 nm: (s.resolutions || "") + " [" + (s.format || "UNKNOWN") + "]",
+                Url: s.url || "",
+                refer: REFER,
+                Header: s.signCookie ? [{
+                    name: "Cookie",
+                    value: s.signCookie
+                }] : [],
+                Subtitle: subs
+            });
+        });
+    }
+
+    return out;
+}
+
+// ============================================
+// DOWNLOAD HANDLER - ALAG CLASS
+// ============================================
+function handleDownload(req) {
+    let p = (req.id || "").split("--");
+    if (p.length != 2) return [];
+
+    let f = p[0].split("-");
+    if (f.length != 3) return [];
+
+    let sid = f[0], se = parseInt(f[1]), ep = parseInt(f[2]);
+
+    let play = callApi(API + "/play-info?subjectId=" + sid + "&se=" + se + "&ep=" + ep, "GET");
+
+    let subs = [], id = null;
+
+    if (play && play.code == 0 && play.data && play.data.streams) {
+        if (play.data.streams.length) id = play.data.streams[0].id;
+    }
+
+    if (id) {
+        let c = callApi(API + "/get-stream-captions?subjectId=" + sid + "&streamId=" + id, "GET");
+
+        if (c && c.code == 0 && c.data && c.data.extCaptions)
+            c.data.extCaptions.forEach(x => subs.push({ name: x.lan || "", link: x.url || "" }));
+
+        if (!subs.length) {
+            c = callApi(API_RES + "/get-ext-captions?subjectId=" + sid + "&resourceId=" + id, "GET");
+            if (c && c.code == 0 && c.data && c.data.extCaptions)
+                c.data.extCaptions.forEach(x => subs.push({ name: x.lan || "", link: x.url || "" }));
+        }
+    }
+
+    let out = [];
+
+    if (play && play.code == 0 && play.data && play.data.streams) {
+        play.data.streams.forEach(s => {
+            out.push({
+                Type: "2",
+                nm: (s.resolutions || "") + " [DOWNLOAD]",
                 Url: s.url || "",
                 refer: REFER,
                 Header: s.signCookie ? [{
